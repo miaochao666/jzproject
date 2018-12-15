25e18a4b213d8bec14c38353711e0dcf852bbbef
miaochao <1446871885@qq.com>
2018-12-15 15:30:32 +0800

    项目搭建

---
+++ src/main/java/com/uinnove/jzproject/JzprojectApplication.java
@@ -0,0 +1,14 @@
+package com.uinnove.jzproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JzprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JzprojectApplication.class, args);
+	}
+
+}
+
