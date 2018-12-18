6ad5ed4810b2221e9b67717f23241117e2e1d39c
miaochao <1446871885@qq.com>
2018-12-18 13:32:01 +0800

    没怎么动

--- src/main/java/com/uinnove/jzproject/mapper/ModelMapper.java
+++ src/main/java/com/uinnove/jzproject/mapper/ModelMapper.java
@@ -3,8 +3,10 @@ package com.uinnove.jzproject.mapper;
 
 import com.uinnove.jzproject.domain.entity.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ModelMapper extends JpaRepository<Model,String> {
+
 }
